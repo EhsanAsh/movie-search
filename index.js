92ee319514130d9984f458fcc0e2ee8b10adcef1
EhsanAsh <ehsan.ashrafipour@gmail.com>
2024-10-15 19:50:24 -0400

    Search a movie
    Apply a fetch data using axios to search movies based on user input

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('Hi there!');
+const fetchData = async () => {
+	const response = await axios.get('http://www.omdbapi.com/', {
+		params: {
+			apikey: 'f192845',
+			s: 'avengers',
+		},
+	});
+	console.log(response.data);
+};
+
+fetchData();

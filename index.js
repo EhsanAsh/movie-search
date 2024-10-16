64ea17c1f5e80397015575d29c63588b64685983
EhsanAsh <ehsan.ashrafipour@gmail.com>
2024-10-15 20:46:17 -0400

    Fix movie search query and add movie details retrieval

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const fetchData = async () => {
 	const response = await axios.get('http://www.omdbapi.com/', {
 		params: {
 			apikey: 'f192845',
-			s: 'avengers',
+			// s: 'avengers',
+			i: 'tt0848228',
 		},
 	});
 	console.log(response.data);

1bd5137384958f18a8a3b3fc5ca04d53f50b2e34
EhsanAsh <ehsan.ashrafipour@gmail.com>
2024-10-16 21:47:13 -0400

    perf(autocomplete):Add autocomplete functionality and movie summary display

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import { debounce } from './utils.js';
+// import {autocomplete} from './autocomplete.js';
 
 const fetchData = async (searchTerm) => {
 	const response = await axios.get('http://www.omdbapi.com/', {
@@ -32,6 +33,7 @@ root.innerHTML = `
 const input = document.querySelector('input');
 const dropdown = document.querySelector('.dropdown');
 const resultsWrapper = document.querySelector('.results');
+const summary = document.querySelector('#summary');
 
 const onInput = async (event) => {
 	const movies = await fetchData(event.target.value);
@@ -56,6 +58,12 @@ const onInput = async (event) => {
 		${movie.Title}
 		`;
 
+		option.addEventListener('click', () => {
+			dropdown.classList.remove('is-active');
+			input.value = movie.Title;
+			onMovieSelect(movie);
+		});
+
 		resultsWrapper.appendChild(option);
 	}
 };
@@ -65,7 +73,61 @@ input.addEventListener('input', debounce(onInput, 800));
 // Close the dropdown when the user clicks outside of it.
 // we are adding an event listener to the document which is the whole page, to check for any clicks that is not on the root element that we have created.
 document.addEventListener('click', (event) => {
+	// contains returns true if the target element is contained within the root element
+	// contains is a method that is available on the Element interface
 	if (!root.contains(event.target)) {
 		dropdown.classList.remove('is-active');
 	}
 });
+
+const onMovieSelect = async (movie) => {
+	const id = movie.imdbID;
+	const response = await axios.get('http://www.omdbapi.com/', {
+		params: {
+			apikey: 'f192845',
+			i: id,
+		},
+	});
+
+	const movieDetails = response.data;
+	summary.innerHTML = movieTemplate(movieDetails);
+};
+
+const movieTemplate = (movieDetail) => {
+	return `
+		<article class="media">
+      <figure class="media-left">
+        <p class="image">
+          <img src="${movieDetail.Poster}" />
+        </p>
+      </figure>
+      <div class="media-content">
+        <div class="content">
+          <h1>${movieDetail.Title}</h1>
+          <h4>${movieDetail.Genre}</h4>
+          <p>${movieDetail.Plot}</p>
+        </div>
+      </div>
+    </article>
+		<article class="notification is-primary">
+      <p class="title">${movieDetail.Awards}</p>
+      <p class="subtitle">Awards</p>
+    </article>
+    <article class="notification is-primary">
+      <p class="title">${movieDetail.BoxOffice}</p>
+      <p class="subtitle">Box Office</p>
+    </article>
+    <article class="notification is-primary">
+      <p class="title">${movieDetail.Metascore}</p>
+      <p class="subtitle">Metascore</p>
+    </article>
+    <article class="notification is-primary">
+      <p class="title">${movieDetail.imdbRating}</p>
+      <p class="subtitle">IMDB Rating</p>
+    </article>
+    <article class="notification is-primary">
+      <p class="title">${movieDetail.imdbVotes}</p>
+      <p class="subtitle">IMDB Votes</p>
+    </article>
+	`;
+};

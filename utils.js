dd99009ca961323714a48fe4b1986542ca5b966b
EhsanAsh <ehsan.ashrafipour@gmail.com>
2024-10-18 19:59:57 -0400

    perf: Refactor runComparison function to improve readability and performance

--- utils.js
+++ utils.js
@@ -1,1 +1,23 @@
+export const runComparison = () => {
+	const leftSideStats = document.querySelectorAll(
+		'#left-summary .notification'
+	);
+	const rightSideStats = document.querySelectorAll(
+		'#right-summary .notification'
+	);
 
+	leftSideStats.forEach((leftStat, idx) => {
+		const rightStat = rightSideStats[idx];
+
+		const leftSideValue = parseInt(leftStat.dataset.value);
+		const rightSideValue = parseInt(rightStat.dataset.value);
+
+		if (rightSideValue > leftSideValue) {
+			leftStat.classList.remove('is-primary');
+			leftStat.classList.add('is-warning');
+		} else {
+			rightStat.classList.remove('is-primary');
+			rightStat.classList.add('is-warning');
+		}
+	});
+};
